75b49ecadb6b2aef42f11bdc5b73bdbcaae26a9e
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-08-15 17:33:40 +0200

    docs: change text in button

--- src/content/tutorial/1-basics/3-shortcuts/6-dynamic-shortcuts-using-theme-with-CSS-variables/_files/App.svelte
+++ src/content/tutorial/1-basics/3-shortcuts/6-dynamic-shortcuts-using-theme-with-CSS-variables/_files/App.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <div style="--hue: {hueNumber}">
-	<button onclick={() => (hueNumber = 70)} class="fancy-button-random">Make me orange</button>
+	<button onclick={() => (hueNumber = 70)} class="fancy-button-random">Click me! (orange)</button>
 
-	<button onclick={() => (hueNumber = 150)} class="fancy-button-random">Make me green</button>
+	<button onclick={() => (hueNumber = 150)} class="fancy-button-random">Click me! (green)</button>
 
-	<button onclick={() => (hueNumber = 200)} class="fancy-button-random">Make me blue</button>
+	<button onclick={() => (hueNumber = 200)} class="fancy-button-random">Click me! (blue)</button>
 </div>
