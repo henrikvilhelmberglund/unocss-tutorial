3a095d957b5a03372af05fba695dd602c7fded45
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2024-08-15 17:35:36 +0200

    feat: use runtime to improve hmr (#1)
    * docs: add .bugs.todo
    
    * docs: change text in button
    
    * feat: add reset to index.html
    
    * feat: use runtime instead of vite plugin for better HMR performance

--- src/templates/default/vite.config.js
+++ src/templates/default/vite.config.js
@@ -1,8 +1,7 @@
 // vite.config.ts
-import UnoCSS from "unocss/vite";
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
-  plugins: [svelte(), UnoCSS()],
+  plugins: [svelte()],
 });
