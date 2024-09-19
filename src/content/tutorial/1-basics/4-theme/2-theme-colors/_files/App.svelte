680450fa6488a2ebc7da8439f3a031506d0ede29
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-09-19 16:49:57 +0200

    fix: button to p, add text

--- src/content/tutorial/1-basics/4-theme/2-theme-colors/_files/App.svelte
+++ src/content/tutorial/1-basics/4-theme/2-theme-colors/_files/App.svelte
@@ -1,8 +1,8 @@
-<button class="text-very-cool">This text is very cool</button>
+<p class="text-very-cool">This text is very cool</p>
 
 <div class="[--hue:250]">
 	<h1 class="text-3xl text-random-500">Using theme colors</h1>
 	<aside class="bg-random-400 size-64 hover:[--hue:150]">
-		<p class="text-random-50">Hover this square to change the color!</p>
+		<p class="text-random-50">Hover this square to change the color of the square!</p>
 	</aside>
 </div>
