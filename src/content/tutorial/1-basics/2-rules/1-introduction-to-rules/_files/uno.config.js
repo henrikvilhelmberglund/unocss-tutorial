5b150674b2550a322e8bc61da586956026cdf3cd
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-09-19 16:41:13 +0200

    fix: remove introduction with files (without a solution) because it didn't seem useful

--- src/content/tutorial/1-basics/2-rules/1-introduction-to-rules/_files/uno.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "@unocss/runtime";
-import presetUno from "https://esm.sh/@unocss/preset-uno";
-
-export default defineConfig({
-	presets: [presetUno()],
-	rules: [],
-});
